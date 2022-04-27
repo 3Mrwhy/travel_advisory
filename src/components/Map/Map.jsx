fdeef7e2e9f7c92154b59bef71b1dda33cb52204
Sudhir <3mrwhy@gmail.com>
2022-04-27 14:17:22 +0530

    Some Css changes

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutLined';
 const Map = () => {
     return(
         <h1> Map </h1>
